24ac1ac23168710a9e501c852dc839a0e75e5a0a
trindadedev13 <devsuay@gmail.com>
2024-10-24 16:17:37 -0300

    feat: better android theme

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -36,6 +36,7 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+            implementation(libs.androidx.core.splashscreen)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
